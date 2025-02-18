a51ba371efc18e843ead54ebf218476daab6ec66
Hafid <hafid@gmail.com>
2025-02-19 06:33:15 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const { PORT = 8000 } = process.env;
 
+const router = require("./routes");
+const { CustomError, errorHandler } = require("./utils/errorHandler");
+
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -20,6 +23,16 @@ app.get("/", (req, res, next) => {
     next(err);
   }
 });
+
+app.use('/api/v1',router);
+
+// 404 error handling
+app.use((req, res, next) => {
+  next(new CustomError(404, "Not Found"));
+});
+
+// 500 Internal Server Error
+app.use(errorHandler);
 
 app.listen(PORT, () =>
   console.log(`server running at http://localhost:${PORT}`)
