4532e788619c4e790a82887abf9f086479957270
Hafid <hafid@gmail.com>
2025-02-19 21:26:53 +0700

    modified app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const routes = require("./routes");
 const authMiddleware = require("./middleware/auth.middleware");
 const { PORT = 8000 } = process.env;
+const { CustomError, errorHandler } = require("./utils/errorHandler");
 
 const app = express();
-
-const router = require("./routes");
-const { CustomError, errorHandler } = require("./utils/errorHandler");
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -19,10 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Apply auth middleware to all protected routes
-app.use('/api/protected', authMiddleware);
+app.use("/api/protected", authMiddleware);
 
 // Load all routes
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Root route
 app.get("/", (req, res) => {
@@ -33,20 +31,22 @@ app.get("/", (req, res) => {
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Something went wrong!",
-  });
-});
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({
+//     success: false,
+//     message: "Something went wrong!",
+//   });
+// });
+
 // 404 error handling
 app.use((req, res, next) => {
   next(new CustomError(404, "Not Found"));
 });
+
 // 500 Internal Server Error
 app.use(errorHandler);
 
 app.listen(PORT, () =>
-    console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 );
