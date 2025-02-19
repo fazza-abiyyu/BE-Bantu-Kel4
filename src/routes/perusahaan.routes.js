a340f65d3cccb3ef0cde49297c0c84577db6525e
Hafid <hafid@gmail.com>
2025-02-19 21:26:09 +0700

    feat : update routes perusahaan

--- src/routes/perusahaan.routes.js
+++ src/routes/perusahaan.routes.js
@@ -2,15 +2,18 @@ const router = require("express").Router();
 const {
   registerPerusahaan,
   editPerusahaan,
-} = require("../controllers/perusahaan.controllers");
+  loginPerusahaan,
+} = require("../controllers/perusahaan.controller");
 const { image } = require("../libs/multer");
+const authMiddleware = require("../middleware/company.middleware");
 
 router.get("/", (req, res, next) => {
   return res.status(200).json({
     data: "succres",
   });
 });
-router.post("/register", image.single("image"), registerPerusahaan);
-router.put("/:id", image.single("image"), editPerusahaan);
+router.post("/company/register", image.single("image"), registerPerusahaan);
+router.post("/company/login", loginPerusahaan);
+router.put("/company/edit", authMiddleware,image.single("image"), editPerusahaan);
 
 module.exports = router;
